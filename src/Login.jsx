76cb32b8a18a7d963b32e3174a9d478f99af2793
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-03-01 00:28:58 +0100

    Login page with complete css and app logo

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,8 +8,9 @@ export default function Login() {
     //const [cat,setCat] = useState('')
   
     return <div className="hid">
-        <form >
-    <h3>Welcome</h3>
+      <form >
+    <img src="/SEA-logo.png" alt='Logo of the app'></img>
+    <h2>Welcome</h2>
     <input
       type="username" required placeholder="username" maxLength="50"
       name='username' />
@@ -17,6 +18,8 @@ export default function Login() {
       type="password" required placeholder="Password (min 6 letters)" minLength="6"
       name="password" maxLength="50" />
     <button type="submit">LOGIN</button>
-  </form>;
+  </form>
+  <div className='wrong-input'><p>Forgot <span>Username / Password</span></p>
+  <p>Contact customer care at <span>eazylivinghq@gmail.com</span></p></div>
     </div>
 }
