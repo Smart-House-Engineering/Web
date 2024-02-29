e8b813a904ecd26bd1f225481ad2c8744dba3a37
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-02-29 08:48:33 +0100

    Committing changes to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login';
+
 
 function App() {
  // const [count, setCount] = useState(0)
@@ -9,7 +11,8 @@ function App() {
   return (
     <>
     <div>
-      <h3>Eazy Living</h3></div>  
+      <Login/>
+      </div>  
     </>
   )
 }
