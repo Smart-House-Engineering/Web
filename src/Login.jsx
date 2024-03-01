0bd1620c51a6b57f5fc653d6a565043641d59b77
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-03-01 18:57:43 +0100

    handling Login input

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,22 +4,49 @@ import './login.css'
 
 
 export default function Login() {
+
+  const [i, setI] = useState({
+    email:'',
+    password:''
+  }); //user info object
+
+  const setUI = (key, value) => setI({ ...i, [key]: value }); //setUI sets UserInfo value and update i value.
+
+  function setStateFromForm(event) {
+    let element = event.target;
+    setUI(element.name, element.value);
+    //console.log(i)
+    
+   
+  }
+
+  async function submit(event) {
+    // prevent submit from doing a hard page reload
+    event.preventDefault();
+    console.log(i)
+    
+    
+  }
+
+  //On submit
+  //set it to the object and then fetch from api to confirm. If it is correct (console.log), move on to the next
       
-    //const [cat,setCat] = useState('')
   
     return <div className="hid">
-      <form >
+      <form onSubmit={submit} >
     <img src="/SEA-logo.png" alt='Logo of the app'></img>
     <h2>Welcome</h2>
     <input
-      type="username" required placeholder="username" maxLength="50"
-      name='username' />
+      type="email" required placeholder="email" maxLength="50"
+      name='email' value={i.email}
+      onChange={setStateFromForm}  />
     <input
       type="password" required placeholder="Password (min 6 letters)" minLength="6"
-      name="password" maxLength="50" />
+      name="password" maxLength="50" value={i.password}
+      onChange={setStateFromForm}  />
     <button type="submit">LOGIN</button>
   </form>
-  <div className='wrong-input'><p>Forgot <span>Username / Password</span></p>
+  <div className='wrong-input'><p>Forgot <span>Email / Password</span></p>
   <p>Contact customer care at <span>eazylivinghq@gmail.com</span></p></div>
     </div>
 }
