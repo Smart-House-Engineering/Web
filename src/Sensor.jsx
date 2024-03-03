c0847299c45ca126194e74601b2403ccac01f262
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-03-03 20:16:26 +0100

    new changes

--- src/Sensor.jsx
+++ src/Sensor.jsx
@@ -1,21 +1,60 @@
+import { useState } from 'react'
+import "./default-page.css";
+
+
 export default function Sensor(props) {
+
     let { keyName, value} = props;
-    let like = String(value)
+    const [like, setLike] = useState(value);
+     
     console.log('Sensor Component Props:', props)
     //let like = value
     //console.log(sense)
 
     //console.log(like)
-    function Switch () {
+   /* function Switch () {
 
-    }
+
+    }*/
+
+    const Switch = async () => {
+        try{ setLike(!like);
+
+            let device = { [keyName]: !like };
+            console.log(device)
+            console.log(typeof device)
+
+            // Send the updated state to the backend
+            const serverResponse = await fetch("http://localhost:5000/api/modes/defaultMode", {
+              method: "PUT",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": true,
+              },
+              body: JSON.stringify(device), // Toggle the like state
+            });
+      
+            if (!serverResponse.ok) {
+              throw new Error(`HTTP error! status: ${serverResponse.status}`);
+            }
+      
+            const response = await serverResponse.json();
+            console.log("Response from server", response);
+          } catch (error) {
+            console.error("Error:", error.message);
+          }
+
+        
+        
+      };
 
     return(
-        <div className='caption'>
-            <div>{like}</div>
+        <div className='caption' onClick = {Switch}>
             <div>{keyName}</div>
+            <div>{String(like)}</div>
 
-      leader
+      
       
       </div>
     )
