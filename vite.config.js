e3b8275afc029a81c13398a73c17de8005f6e296
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-03-05 03:51:24 +0100

    latest update

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-import { reactEasierViteConfig as revc_ } from 'react-easier/vite-config'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(revc_({
-  plugins: [react()],
-}))
+
 
 
 // https://vitejs.dev/config/
-/*export default defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'https://backend-ten-ruby.vercel.app',
         changeOrigin: true,
         secure: true
       }
     }
   }
-})*/
+})
 
