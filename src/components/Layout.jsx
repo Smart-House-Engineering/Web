7234cb8474391710d818795708de638930543209
raudazurak <99649820+raudazurak@users.noreply.github.com>
2024-05-14 22:50:37 +0200

    adding emergency mode

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "./sidebar";
+import { useState } from "react";
+
 
 function PageWithSidebar({ mainComponent: MainComponent }) {
+
+  
   return (
     <div style={{ display: "flex" }}>
-      <Sidebar />
+      <Sidebar/>
       <main style={{ flex: 1 }}>
         <MainComponent />
       </main>
---
+++ src/components/emergencyMode.jsx
@@ -0,0 +1,90 @@
+import "../style/default-page.css";
+import "../style/emergency.css";
+import { useState, useEffect } from "react";
+
+
+
+
+export default function EmergencyMode() {
+
+  const [isToggled, setIsToggled] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetch(
+          "https://evanescent-beautiful-venus.glitch.me/api/modes/otherModes",
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Credentials": true,
+            },
+            mode: "cors",
+          }
+        );
+        let data = await response.json();
+        console.log("response", data.modes.emergency);
+        if (data.modes.emergency === true) {
+          setIsToggled(true);
+         
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []); 
+
+  
+  
+  
+  const handleToggle = async () => {
+    try {
+      // Toggle isToggled state
+      const updatedIsToggled = !isToggled;
+      setIsToggled(updatedIsToggled);
+
+      // Send the updated state to the backend
+      const serverResponse = await fetch(
+        "https://evanescent-beautiful-venus.glitch.me/api/homeUser/otherModes",
+        {
+          method: "PUT",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+          },
+          cookies: localStorage.getItem("SmartHouseToken"),
+          body: JSON.stringify({ updatedModes: { emergency: updatedIsToggled } }), // Toggle the like state
+        }
+      );
+
+
+
+
+      if (!serverResponse.ok) {
+        throw new Error(`HTTP error! status: ${serverResponse.status}`);
+      }
+
+      const response = await serverResponse.json();
+
+      console.log("response", response);
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+    return(
+    <div className="home-icon">
+    <label className="switch" checked={isToggled} onChange={handleToggle}>
+    <input type="checkbox"  checked={isToggled} onChange={handleToggle} />
+    <span className="slider round"></span>
+    </label>
+    <p>emergency mode</p>
+  </div>
+ )
+
+
+}
