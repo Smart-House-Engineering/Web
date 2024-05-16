81ef4c14bd1e050e16695dab5e2774a5fd8e1c16
Liis U <liis.usin@gmail.com>
2024-05-16 22:06:57 +0200

    config file, and gitignore updates

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,12 @@
 // jest.config.cjs or jest.config.js
 module.exports = {
-  testEnvironment: "jsdom",
-  testMatch: ["**/tests/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
+    testEnvironment: "jsdom",
+    testMatch: ["**/tests/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+    },
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    },
+    moduleFileExtensions: ["js", "jsx"],
 };
