37e662c641b2ddfd2f13b4b7194b57e5d80bbe62
Liis U <liis.usin@gmail.com>
2024-05-16 23:59:51 +0200

    add testid to authorize.js

--- src/pages/unauthorize.js
+++ src/pages/unauthorize.js
@@ -4,19 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/authContext";
 
 const Unauthorized = () => {
-  const navigate = useNavigate();
-  const { setIsLoggedIn, setAuthUser, authUser } = useAuth();
-  console.log("auth user", authUser);
-  return (
-    <div className="unauthorized-container">
-      <img src="/lock.png" alt="Lock Icon" className="lock-icon" />
-      <h1>Unauthorized Access</h1>
-      <p>You are not authorized to view this page. Please login to continue.</p>
-      <button onClick={() => navigate("/")} className="login-button">
-        Login
-      </button>
-    </div>
-  );
+    const navigate = useNavigate();
+    const { setIsLoggedIn, setAuthUser, authUser } = useAuth();
+    console.log("auth user", authUser);
+    return (
+        <div className="unauthorized-container" data-testid="unauthorized">
+            <img src="/lock.png" alt="Lock Icon" className="lock-icon" />
+            <h1>Unauthorized Access</h1>
+            <p>
+                You are not authorized to view this page. Please login to
+                continue.
+            </p>
+            <button onClick={() => navigate("/")} className="login-button">
+                Login
+            </button>
+        </div>
+    );
 };
 
 export default Unauthorized;
