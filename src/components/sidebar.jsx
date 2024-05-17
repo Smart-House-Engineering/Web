1547ed2f92e3de347488bce59761f9281a41eb42
Liis U <liis.usin@gmail.com>
2024-05-18 01:37:41 +0200

    sidebar tests

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,47 +4,57 @@ import "react-circular-progressbar/dist/styles.css";
 import { useAuth } from "../utils/authContext";
 
 function Sidebar() {
-  const navigate = useNavigate();
-  const { setIsLoggedIn, setAuthUser, authUser } = useAuth();
-  const logout = async () => {
-    setAuthUser(null);
-    setIsLoggedIn(false);
-    navigate("/");
-  };
+    const navigate = useNavigate();
+    const { setIsLoggedIn, setAuthUser, authUser } = useAuth();
+    const logout = async () => {
+        setAuthUser(null);
+        setIsLoggedIn(false);
+        navigate("/");
+    };
 
-  return (
-    <div className="sidebar">
-      <img src="/SEA-logo.png" alt="Logo of the app"></img>
-      <div className="home-icon" onClick={() => navigate("/default")}>
-        <img src="/home-2.svg" alt="Home or default page"></img>
-        <p>Home</p>
-      </div>
-      <div className="home-icon">
-        <img src="/modes.svg" alt="Modes page"></img>
-        <p>Modes</p>
-      </div>
-      <div className="home-icon">
-        <img src="/microphone-2.svg" alt="Voice recognition"></img>
-        <p>Voice</p>
-      </div>
-      {authUser?.role === "OWNER" ? (
-        <>
-          <div className="home-icon" onClick={() => navigate("/add-user")}>
-            <img src="/logged.svg" alt="Logout"></img>
-            <p> Add user</p>
-          </div>
-          <div className="home-icon" onClick={() => navigate("/delete-user")}>
-            <img src="/logged.svg" alt="Logout"></img>
-            <p> Delete user</p>
-          </div>
-        </>
-      ) : null}
-      <div className="home-icon" onClick={logout}>
-        <img src="/logged.svg" alt="Logout"></img>
-        <p>Logout</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="sidebar">
+            <img src="/SEA-logo.png" alt="Logo of the app"></img>
+            <div className="home-icon" onClick={() => navigate("/default")}>
+                <img src="/home-2.svg" alt="Home or default page"></img>
+                <p>Home</p>
+            </div>
+            <div className="home-icon">
+                <img src="/modes.svg" alt="Modes page"></img>
+                <p>Modes</p>
+            </div>
+            <div className="home-icon">
+                <img src="/microphone-2.svg" alt="Voice recognition"></img>
+                <p>Voice</p>
+            </div>
+            {authUser?.role === "OWNER" ? (
+                <>
+                    <div
+                        className="home-icon"
+                        onClick={() => navigate("/add-user")}
+                    >
+                        <img src="/logged.svg" alt="Logout"></img>
+                        <p> Add user</p>
+                    </div>
+                    <div
+                        className="home-icon"
+                        onClick={() => navigate("/delete-user")}
+                    >
+                        <img src="/logged.svg" alt="Logout"></img>
+                        <p> Delete user</p>
+                    </div>
+                </>
+            ) : null}
+            <div
+                className="home-icon"
+                onClick={logout}
+                data-testid="logout-test"
+            >
+                <img src="/logged.svg" alt="Logout"></img>
+                <p>Logout</p>
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
---
+++ src/tests/components/sidebar.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
+import { AuthProvider } from "../../utils/authContext";
+import "@testing-library/jest-dom";
+
+jest.mock("../../utils/authContext", () => ({
+    ...jest.requireActual("../../utils/authContext"),
+    useAuth: jest.fn(() => ({
+        setIsLoggedIn: jest.fn(),
+        setAuthUser: jest.fn(),
+        authUser: {
+            email: "test@example.com",
+            homeId: "myHome",
+            role: "OWNER",
+        },
+    })),
+}));
+
+describe("Sidebar component", () => {
+    beforeEach(() => {
+        render(
+            <AuthProvider>
+                <BrowserRouter>
+                    <Sidebar />
+                </BrowserRouter>
+            </AuthProvider>
+        );
+    });
+
+    test("renders sidebar elements", () => {
+        const homeLink = screen.getByText("Home");
+        expect(homeLink).toBeInTheDocument();
+    });
+
+    test("navigates to default page when home icon is clicked", () => {
+        const homeIcon = screen.getByAltText("Home or default page");
+        fireEvent.click(homeIcon);
+        expect(window.location.pathname).toBe("/default");
+    });
+
+    test("logs out when logout icon is clicked", () => {
+        const logoutIcon = screen.getByTestId("logout-test");
+        fireEvent.click(logoutIcon);
+        expect(window.location.pathname).toBe("/");
+    });
+});
